baf5577d79bcdf8a5a28fee21912622a2adf0871
KyawThura <kyawthura.softstartechnology@gmail.com>
2026-04-30 21:40:11 +0630

    updated add gitignore file and changed vi editor

--- src/main/EcommerceMain.java
+++ src/main/EcommerceMain.java
@@ -6,6 +6,7 @@ public class EcommerceMain {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git Test");
 		System.out.println("Changed ");
+		System.out.println("Modified Stage");
 	}
 
 }
